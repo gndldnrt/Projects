116a35f14da95f8a1d218089cd9d87c29f82e033
Macbook Pro M1 <macbookprom1@Macbooks-MacBook-Pro.local>
2026-08-30 00:45:34 +0800

    Initial commit

---
+++ Python Files/Demo.py
@@ -0,0 +1,8 @@
+# print("Hello, World!")
+name = "Geandell"
+age = 25
+is_Developer = True
+
+print("My name is", name, "and I am", age, "years old.")
+if is_Developer:
+    print("I am a developer.")
